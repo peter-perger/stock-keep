53fd973f0ec993415543d0496ff8f7a20a507d2a
Peter Perger <pergerpeti2@gmail.com>
2026-05-27 20:58:12 +0200

    Created the RegisterForm

---
+++ stKeepProject/stKeepApp/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class RegisterForm(forms.ModelForm):
+    username = forms.CharField(max_length=150, help_text="")
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput, label="Confirm password")
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password", "password_confirm"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password and confirm_password and password != confirm_password:
+            raise forms.ValidationError("Passwords do not match!")
+        
+        return cleaned_data
+

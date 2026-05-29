64eace228d7bdd23ff34df347819b672bd54a1b9
Peter Perger <pergerpeti2@gmail.com>
2026-05-29 15:39:55 +0200

    Created Product model and migrated to teh database

--- stKeepProject/stKeepApp/models.py
+++ stKeepProject/stKeepApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    supplier = models.CharField(max_length=150)
+
+    def _str__(self):
+        return {f"{self.name}, {self.supplier}"}
+
+    

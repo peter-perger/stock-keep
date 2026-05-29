5bab38628d55fec5d3a44ab676f8cafcc16f2ae6
Peter Perger <pergerpeti2@gmail.com>
2026-05-29 13:29:38 +0200

    Added the path for the profile page

--- stKeepProject/stKeepApp/urls.py
+++ stKeepProject/stKeepApp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index_view, name='index'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
-    path('logout', views.logout_view, name='logout')
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.ProfileView.as_view(), name='profile')
 ]
